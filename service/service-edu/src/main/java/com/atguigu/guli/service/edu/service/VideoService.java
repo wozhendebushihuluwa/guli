aee6a84192c6a8f9b24893fb5521e66c98edb6c6
zhanghuixin <936338975@qq.com>
2020-02-12 15:59:07 +0800

    在线教育day02上午

---
+++ service/service-edu/src/main/java/com/atguigu/guli/service/edu/service/VideoService.java
@@ -0,0 +1,16 @@
+package com.atguigu.guli.service.edu.service;
+
+import com.atguigu.guli.service.edu.entity.Video;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程视频 服务类
+ * </p>
+ *
+ * @author ZHX
+ * @since 2020-02-12
+ */
+public interface VideoService extends IService<Video> {
+
+}
