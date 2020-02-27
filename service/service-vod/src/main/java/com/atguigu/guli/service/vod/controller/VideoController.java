7ea35096b1eaee7f625a73bba524cb9c1477904f
zhanghuixin <936338975@qq.com>
2020-02-27 19:06:46 +0800

    在线教育后端系统第十二天

---
+++ service/service-vod/src/main/java/com/atguigu/guli/service/vod/controller/VideoController.java
@@ -0,0 +1,47 @@
+package com.atguigu.guli.service.vod.controller;
+
+import com.atguigu.guli.common.base.result.R;
+import com.atguigu.guli.common.base.result.ResultCodeEnum;
+import com.atguigu.guli.common.base.util.ExceptionUtils;
+import com.atguigu.guli.service.base.exception.GuliException;
+import com.atguigu.guli.service.vod.service.VideoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@RestController
+@RequestMapping("admin/vod/video")
+@CrossOrigin
+@Api(description = "阿里云视频点播")
+@Slf4j
+public class VideoController {
+    @Autowired
+    private VideoService videoService;
+
+
+    @ApiOperation(value = "阿里云视频上传")
+    @PostMapping("upload")
+    public R uploadVideo(
+            @ApiParam(name = "file",value = "视屏文件",required = true)
+            @RequestParam("file")MultipartFile file
+            ){
+        try {
+            String originalFilename = file.getOriginalFilename();
+            InputStream inputStream = file.getInputStream();
+            String videoId = this.videoService.uploadVideo(inputStream, originalFilename);
+            return R.ok().message("视频上传成功").data("videoId",videoId);
+        } catch (IOException e) {
+            log.error(ExceptionUtils.getMessage(e));
+            throw new GuliException(ResultCodeEnum.VIDEO_UPLOAD_TOMCAT_ERROR);
+        }
+
+    }
+
+}
---
+++ service/service-vod/src/main/java/com/atguigu/guli/service/vod/util/VodProperties.java
@@ -0,0 +1,14 @@
+package com.atguigu.guli.service.vod.util;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "aliyun.vod.file")
+public class VodProperties {
+     private String keyid;
+     private String keysecret;
+}
+
